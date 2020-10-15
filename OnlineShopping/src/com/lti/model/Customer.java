5dedf8930578c5ab3286c357bdf9c544fa52d4d7
VIGANESHA <VIGANESHA@DESKTOP-1AAARUT>
2020-10-15 15:45:28 +0530

    first commit

---
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Customer {
+	int custId;
+	String custName;
+	public int getCustId() {
+		return custId;
+	}
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	
+}
