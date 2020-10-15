ce0c0898abf1fcc78e4e0cf2aaafd0666f8e11eb
VIGANESHA <VIGANESHA@DESKTOP-1AAARUT>
2020-10-15 15:50:00 +0530

    made changes in cust class

--- OnlineShopping/src/com/lti/model/Customer.java
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -3,6 +3,13 @@ package com.lti.model;
 public class Customer {
 	int custId;
 	String custName;
+	String custCity;
+	public String getCustCity() {
+		return custCity;
+	}
+	public void setCustCity(String custCity) {
+		this.custCity = custCity;
+	}
 	public int getCustId() {
 		return custId;
 	}
